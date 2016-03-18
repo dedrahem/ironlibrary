a56b75c166083286f4591ed8186ba8d25546a7a0
douglas drahem <drahemde@gmail.com>
2016-03-17 22:47:11 -0500

    styling majority of it is done, remaining tasks per the original assignment add the photo url and display the price somewhere, price is in the new seed file, items 4 and 5 from the assignment need to be done, concerning the readme maddog screen shots mit and heroku.  sounds like a lot, but may not actually be that bad.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,13 @@ Book.delete_all
 Author.delete_all
 
 if Author.count == 0
-  john = Author.create first_name: "John", last_name: "Winter", authorbio: "blah blah"
-  frangie = Author.create first_name: "Frangie", last_name: "Pangie", authorbio: "blah"
-  bill = Author.create first_name: "Bill", last_name: "Clinton", authorbio: "blah blah blah"
+  sam = Author.create first_name: 'Sam', last_name: 'Ruby', authorbio: 'Sam Ruby is a prominent software developer who is a co-chair of the W3C HTML Working Group and has made significant contributions to many of the Apache Software Foundation open source software projects. He is a Senior Technical Staff Member in the Emerging Technologies Group of IBM.  Dave Thomas, as one of the authors of the Agile Manifesto, understands agility. As the author of Programming Ruby he understands Ruby. And, as an active Rails developer, he knows Rails.  David Heinemeier Hansson is the creator of the Rails framework.'
+  nick = Author.create first_name: 'Nick', last_name: 'Morgan', authorbio: 'Nick Morgan is a fronted engineer at Twitter.  He loves all programming languages but has a particular soft spot for JavaScript.  Nick lives in San Francisco (the foggy part) with his fiance and their fluffydog, Pancake.  He blogs at skilldrick.co.uk.'
+  eric = Author.create first_name: 'Eric', last_name: 'Weinstein', authorbio: 'Eric Weinstein has helped millions of people learn to program through CodeAcademy, where he designed and authored the Ruby curriculum.  He has also taught creative writing to undergraduates and veterans at NYU, where he was a Veterans Writing Workshop Fellow.  He writes Ruby for a living in New York City.'
 end
 
 if Book.count == 0
-  Book.create title: "Title Field", price: "12.95", author_id: john.id
-  Book.create title: "Another Title Field", price: "11.95", author_id: john.id
-  Book.create title: "this is a title", price: "10.95", author_id: frangie.id
-  Book.create title: "this is a title", price: "7.75", author_id: bill.id
-  Book.create title: "this another title", price: "5.25", author_id: john.id
+  Book.create title: 'Agile Web Development with Rails 4 (Facets of Ruby) 1st Edition', price: '28.71', author_id: sam.id
+  Book.create title: 'JavaScript for Kids A Playful Introduction to Programming', price: '25.58', author_id: nick.id
+  Book.create title: 'Ruby Wizardry An Introduction to Programming for Kids Paperback', price: '23.08', author_id: eric.id
 end
